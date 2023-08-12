84302e18fc8b81ae14283e9bd6083fe24e57f397
phillipkujawa <phillip.kujawa@me.com>
2023-08-12 07:44:21 -0400

    created thoughtRoute

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../controllers/thoughtController');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+
+module.exports = router;
